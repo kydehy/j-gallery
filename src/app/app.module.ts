471c5c3016d1bdf1b8d4f8ed57999dfbe9af9687
kydehy <nich.dev95@gmail.com>
2024-09-13 21:35:43 -0300

    se crea modulo allView (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AllViewModule } from './all-view/all-view.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AllViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
