4c92b996e063b9cd6016a7c74bfb974ae397e8ea
kydehy <nich.dev95@gmail.com>
2024-09-14 19:31:23 -0300

    refactorizacion de directorios

--- src/app/all-view/all-view.module.ts
+++ src/app/all-view/all-view.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
