5b5483e4913b8388b40fcfc9379653f0213896b4
kydehy <nich.dev95@gmail.com>
2024-09-14 19:32:07 -0300

    refactorizacion de directorios (#5)

--- src/app/secciones/components/galerias/galerias.component.spec.ts
+++ src/app/secciones/components/galerias/galerias.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GaleriaComponent } from './galeria.component';
+import { GaleriasComponent } from './galerias.component';
 
-describe('GaleriaComponent', () => {
-  let component: GaleriaComponent;
-  let fixture: ComponentFixture<GaleriaComponent>;
+describe('GaleriasComponent', () => {
+  let component: GaleriasComponent;
+  let fixture: ComponentFixture<GaleriasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GaleriaComponent]
+      declarations: [GaleriasComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(GaleriaComponent);
+    fixture = TestBed.createComponent(GaleriasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
